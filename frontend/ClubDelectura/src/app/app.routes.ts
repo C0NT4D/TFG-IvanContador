343d7c4e97956ccca74a98b3c6fc16341afbbfd4
IvanContador <ivcoso@floridauniversitaria.es>
2025-04-09 11:51:41 +0200

    solucionando error de rutas

--- frontend/ClubDelectura/src/app/app.routes.ts
+++ frontend/ClubDelectura/src/app/app.routes.ts
@@ -12,23 +12,25 @@ import { DetalleComponent as EventoDetalleComponent } from './views/evento/detal
 import { NuevoComponent as EventoNuevoComponent } from './views/evento/nuevo/nuevo.component';
 import { ListadoComponent as RecomendacionListadoComponent } from './views/recomendacion/listado/listado.component';
 import { PerfilComponent } from './views/usuario/perfil/perfil.component';
+import { DetalleComponent as UsuarioDetalleComponent } from './views/usuario/detalle/detalle.component';
 import { authGuard } from './guards/auth.guard';
 import { adminGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/libros', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: 'libros', component: LibroListadoComponent, canActivate: [authGuard] },
-  { path: 'libros/:id', component: LibroDetalleComponent, canActivate: [authGuard] },
+  { path: 'libros', component: LibroListadoComponent },
+  { path: 'libros/:id', component: LibroDetalleComponent },
   { path: 'libros/nuevo', component: LibroNuevoComponent, canActivate: [authGuard] },
-  { path: 'foros', component: ForoListadoComponent, canActivate: [authGuard] },
-  { path: 'foros/:id', component: ForoDetalleComponent, canActivate: [authGuard] },
+  { path: 'foros', component: ForoListadoComponent },
+  { path: 'foros/:id', component: ForoDetalleComponent },
   { path: 'foros/nuevo', component: ForoNuevoComponent, canActivate: [authGuard] },
-  { path: 'eventos', component: EventoListadoComponent, canActivate: [authGuard] },
-  { path: 'eventos/:id', component: EventoDetalleComponent, canActivate: [authGuard] },
+  { path: 'eventos', component: EventoListadoComponent },
+  { path: 'eventos/:id', component: EventoDetalleComponent },
   { path: 'eventos/nuevo', component: EventoNuevoComponent, canActivate: [authGuard] },
-  { path: 'recomendaciones', component: RecomendacionListadoComponent, canActivate: [authGuard] },
+  { path: 'recomendaciones', component: RecomendacionListadoComponent },
   { path: 'perfil', component: PerfilComponent, canActivate: [authGuard] },
-  { path: 'usuarios', loadComponent: () => import('./views/usuario/listado/listado.component').then(m => m.ListadoComponent), canActivate: [adminGuard] }
+  { path: 'usuarios', loadComponent: () => import('./views/usuario/listado/listado.component').then(m => m.ListadoComponent), canActivate: [adminGuard] },
+  { path: 'usuarios/:id', component: UsuarioDetalleComponent, canActivate: [adminGuard] }
 ];
